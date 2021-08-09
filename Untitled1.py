a483d3d572dc7bca27054259c6e64bd81ba7f012
Inesa <87142472+logit02@users.noreply.github.com>
2021-08-09 21:26:16 +0400

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -1,17 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import requests
 from bs4 import BeautifulSoup 
 import pandas as pd
 import time
-
-
-# In[2]:
-
 
 html = requests.get("https://www.spyur.am/am/home/search-1/?company_name=գործարան")
 
@@ -26,8 +16,6 @@ base_link = 'https://www.spyur.am/am/home/search-'
 all_link = [base_link + str(i) + '/?company_name=գործարան' for i in range(1,46)]
 
 
-# In[3]:
-
 
 for i in all_link:
     html = requests.get(i)
@@ -38,33 +26,12 @@ for i in all_link:
             zavod.append(each.get('href'))
 
 
-# In[4]:
-
 
 zavod = list(set(zavod))
 
-
-# In[5]:
-
-
 itemed_link  = ['https://www.spyur.am' + str(i) for i in zavod]
-
-
-# In[6]:
-
-
 names = []
-
-
-# In[195]:
-
-
 final=[]
-
-
-# In[196]:
-
-
 
 from tqdm import tqdm
 
@@ -99,53 +66,22 @@ for z in tqdm(itemed_link):
                         
                         web.append(link)
                         #print(web)
-                
-               # print(al)
-                        
-                    
                
     obj = {"Company_name":anun,
           'Phone':hamar,
           'url' : web}
     
-    
-        
-    
-    
+
     final.append(obj)
     #print("This is your object")
    
-    
-    
-        
-
-
-# In[198]:
 
 
 columns = ['Company_name', 'Phone', 'url']
 
-
-# In[201]:
-
-
 df = pd.DataFrame(final, columns=columns)
 
 
-# In[202]:
-
-
-df
-
-
-# In[205]:
-
-
 df.to_excel('scraped_data.xlsx')
-
-
-# In[ ]:
-
-
 
 
